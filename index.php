fd050a10b13aee018d245a3b788042224238eb52
HeatSink Ru <samueldittedestree@gmail.com>
2021-12-02 10:40:02 +0100

    v1.0.0

--- index.php
+++ index.php
@@ -5,5 +5,12 @@ $web = new Web("E-commerce");
 
 $web->addCSSlink("css/style.css");
 $web->addIcon("https://samsam.go.yo.fr/avatar.png");
+$web->addToBody(<<<HTML
+
+<a class="button is-link">
+    START
+</a>
+
+HTML);
 
 echo $web->toHTML();
